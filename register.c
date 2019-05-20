24d996257a94b6f5f9b24d6e72bef8561893ef5c
YeongHyun Kim <hjj5471@naver.com>
2019-05-20 21:04:33 +0900

    HW06

--- register.c
+++ register.c
@@ -2,31 +2,41 @@
 #include <string.h>
 
 static char passwd[]="qwerty1234";
+static int in_passwd();
 
-void registerPhoneData(){
-	int i=0;
-	char passwd[]="qwerty1234";
-	char in_passwd[50];
-	printf("Enter the password >>");
-	scanf("%s",in_passwd);
-	
-	while(i<3)
+void registerPhoneData()
+{
+	if(in_passwd())
 	{
-		if(!strcmp(passwd,in_passwd)) break;
-		else
-		{
-			i++;
-			if(i==3){
-			printf("you are not allowed to accese PhoneBook.\n");
-			return;
-			}
-			printf("Not matched. Enter the password>>>");
-			scanf("%s",in_passwd);
+		printf("New User Name : ");
+		scanf("%s", PhoneBook[size].Name);
+		printf("Phone Number : ");
+		scanf("%s", PhoneBook[size].PhoneNumber);
+		size+=1;
+	}
+}
+
+int in_passwd()
+{
+	int i=0;
+        char in_passwd[50];
+        printf("Enter the password >>");
+        scanf("%s",in_passwd);
+
+        while(i<3)
+        {
+                if(!strcmp(passwd,in_passwd)) break;
+                else
+                {
+                        i++;
+                        if(i==3)
+			{
+                        printf("you are not allowed to accese PhoneBook.\n");
+                        return 0;
+                        }
+                        printf("Not matched. Enter the password>>>");
+                        scanf("%s",in_passwd);
 		}
 	}
-	printf("New User Name : ");
-	scanf("%s", PhoneBook[size].Name);
-	printf("Phone Number : ");
-	scanf("%s", PhoneBook[size].PhoneNumber);
-	size+=1;
+	return 1;
 }
